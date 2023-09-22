4a5bae3ccb276a9eee13e8b7c492971a367a43a6
hundun <wwj.hddjj@gmail.com>
2023-09-22 11:28:45 +0800

    update html workable; release;

--- html/src/main/java/hundun/gdxgame/textuma/html/client/HtmlLauncher.java
+++ html/src/main/java/hundun/gdxgame/textuma/html/client/HtmlLauncher.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 
+import com.badlogic.gdx.graphics.g2d.freetype.gwt.FreetypeInjector;
+import com.badlogic.gdx.graphics.g2d.freetype.gwt.inject.OnCompletion;
 import hundun.gdxgame.textuma.core.TextUmaGame;
 import hundun.gdxgame.textuma.html.GwtPreferencesSaveTool;
 
@@ -23,5 +25,16 @@ public class HtmlLauncher extends GwtApplication {
     @Override
     public ApplicationListener createApplicationListener() {
         return game;
+    }
+
+    @Override
+    public void onModuleLoad () {
+        FreetypeInjector.inject(new OnCompletion() {
+            public void run () {
+                // Replace HtmlLauncher with the class name
+                // If your class is called FooBar.java than the line should be FooBar.super.onModuleLoad();
+                HtmlLauncher.super.onModuleLoad();
+            }
+        });
     }
 }
